60cda5478f2754c29e5ccfd99b11675d35a18e7d
SamRFreitas <samuelrfreitassi@gmail.com>
2021-05-05 21:06:58 -0300

    Configuração do projeto

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="brand-main">Widget</h1>
+  <div class="w-10 h-10 bg-brand-main">Widget</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default {}
+</script>
+
+<style>
+</style>
